bf68f6ded3546781e60d8ddb06b7d42c9ecc6be1
biaodi <guimashusheng@126.com>
2021-08-09 17:06:09 +0800

    调整stylelint默认值

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,11 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
